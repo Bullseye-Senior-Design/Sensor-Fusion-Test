806d2baa72f78a705780e08f7345b7b1df95a88e
Max <58192471+DiMister@users.noreply.github.com>
2025-10-30 12:11:19 -0500

    pull

--- Robot/Constants.py
+++ Robot/Constants.py
@@ -5,5 +5,5 @@ class Constants:
             self.port = port
             self.offset = offset
 
-    uwb_tag_data = { UWBTagInfo(port="/dev/ttyUSB0", offset=(0.1, 0.0, 0.0)),
-                    UWBTagInfo(port="/dev/ttyUSB1", offset=(0.2, 0.0, 0.0)) }
+    uwb_tag_data = [ UWBTagInfo(port="/dev/ttyUSB0", offset=(0.1, 0.0, 0.0)),
+                    UWBTagInfo(port="/dev/ttyUSB1", offset=(0.2, 0.0, 0.0)) ]
