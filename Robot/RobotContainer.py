0807e5b24e6eec0562b8ede605f4b04b7be0dac9
Sawyer <sawyer.c.theis@gmail.com>
2025-11-19 21:12:47 -0600

    name proper

--- Robot/RobotContainer.py
+++ Robot/RobotContainer.py
@@ -3,7 +3,7 @@ from structure.commands.InstantCommand import InstantCommand
 from structure.commands.SequentialCommandGroup import SequentialCommandGroup
 from Robot.subsystems.sensors.UWB import UWB
 from Robot.subsystems.sensors.IMU import IMU
-from Robot.Commands.LogStateCmd import LogDataCmd
+from Robot.Commands.LogDataCmd import LogDataCmd
 from Robot.Commands.PlotStateCmd import PlotStateCmd
 from Robot.Commands.AlignIMUCmd import AlignIMUCmd
 from Robot.Commands.ZeroIMUCmd import ZeroIMUCmd
