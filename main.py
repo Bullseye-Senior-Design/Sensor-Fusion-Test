62754790380077819d03fd169cddfd68a4f0f23b
Sawyer <sawyer.c.theis@gmail.com>
2026-01-15 16:23:35 -0600

    move the keyboard interrupt to the thread loop

--- main.py
+++ main.py
@@ -14,36 +14,33 @@ def main():
     robot.robot_init()
     robot_state.enable_teleop()
     
-    try:
-        while True:        
-            # Run periodic functions
-            robot.robot_periodic()
-            
-            # Teleop (human operated) mode
-            if robot_state.should_init_teleop():
-                print("Teleop mode enabled")
-                robot.teleop_init()
-            
-            if robot_state.is_teleop_enabled():
-                robot.teleop_periodic()
-            
-            # Test mode
-            if robot_state.should_init_test():
-                print("Test mode enabled")
-                robot.test_init()
-            
-            if robot_state.is_test_enabled():
-                robot.test_periodic()
-            
-            # Disabled check
-            if robot_state.should_init_disable():
-                robot.disabled_init()
-            
-            # Add a small delay to prevent high CPU usage
-            time.sleep(0.01)
-    except KeyboardInterrupt:
-        print("Shutting down robot...")
-        UWB().stop_all()
+    while True:        
+        # Run periodic functions
+        robot.robot_periodic()
+        
+        # Teleop (human operated) mode
+        if robot_state.should_init_teleop():
+            print("Teleop mode enabled")
+            robot.teleop_init()
+        
+        if robot_state.is_teleop_enabled():
+            robot.teleop_periodic()
+        
+        # Test mode
+        if robot_state.should_init_test():
+            print("Test mode enabled")
+            robot.test_init()
+        
+        if robot_state.is_test_enabled():
+            robot.test_periodic()
+        
+        # Disabled check
+        if robot_state.should_init_disable():
+            robot.disabled_init()
+        
+        # Add a small delay to prevent high CPU usage
+        time.sleep(0.01)
+
         
 if __name__ == "__main__":
     main()
