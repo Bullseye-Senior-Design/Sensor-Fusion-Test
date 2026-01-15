042a86cfca50a5e25e4c90d6cf0744c036517fe2
Sawyer <sawyer.c.theis@gmail.com>
2026-01-15 15:34:50 -0600

    commit

--- Debug.py
+++ Debug.py
@@ -4,4 +4,4 @@ class Debug:
     
     # Time scale multiplier for simulation speed (1.0 = real-time, 2.0 = 2x speed, etc.)
     # Affects KalmanStateEstimator, SimIMU, and SimUWB but not main loop
-    time_scale = 5.0
+    time_scale = 1.0
